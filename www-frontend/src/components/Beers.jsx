36c69edc3cdcc201ad158056777f1ad7d14a0649
Chiara Romanini <caromanini@miuandes.cl>
2024-08-29 13:36:25 -0400

    finished beers index with search

--- www-frontend/src/components/Beers.jsx
+++ www-frontend/src/components/Beers.jsx
@@ -38,70 +38,69 @@ const Beers = () => {
     );
 
     return (
-        <div>
-            <Autocomplete
-                freeSolo
-                options={keywordList}
-                value={searchKeywords}
-                onInputChange={handleInputChange}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        label="Search Beer"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                    />
+        <Box display='flex' flexDirection="column" minHeight="100vh">
+            <Box mb={2}>
+                <Autocomplete
+                    freeSolo
+                    options={keywordList}
+                    value={searchKeywords}
+                    onInputChange={handleInputChange}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Search Beer"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            sx={{
+                                width: '200px',
+                                height: '56px'
+                            }}
+                        />
+                    )}
+                />
+            </Box>
+            <Box flex="1">
+                {loading && (
+                    <Typography variant="body1" margin="normal">
+                        Searching for beers...
+                    </Typography>
                 )}
-            />
-            <Button variant="contained" color="primary" onClick={handleSearch}> 
-                Search
-            </Button>
-            {
-                keywordList.length > 0 &&
-                <Button variant="outlined" color="secondary" onClick={handleClearHistory}>
-                    Clear search history
-                </Button>
-            }
-            {loading && (
-                <Typography variant="body1" margin="normal">
-                    Searching for beers...
-                </Typography>
-            )}
-            {error && (
-                <Typography variant="body1" color="error" margin="noraml">
-                    Error fetching data.
-                </Typography>
-            )}
-            {allBeersData && (
-                <List>
-                    {(searchKeywords ? filteredBeers : allBeersData.beers).map((beer, index) => (
-                        <ListItem key={index}>
-                            <div style={{marginBottom: '20px'}}>
-                                <Card>
-                                    <Box sx={{height: 150, display: 'flex', alignItems: 'center', backgroundColor: 'rgb(245, 222, 179)'}}>
-                                        <CardMedia
-                                            component="img"
-                                            sx={{width: 100, height: 100, objectFit: 'cover'}}
-                                            image={beerIcon}
-                                            alt={beer.name}
-                                        />
-                                        <CardContent>
-                                            <Typography variant="h6" component="div" sx={{textAlign: 'left', color: 'rgb(78, 42, 30)'}}>
-                                                {`${beer.name}`}
-                                            </Typography>
-                                            <Typography variant="body2" color="textSecondary" sx={{textAlign: 'left', mt: 1, color: 'rgb(78, 42, 30)'}}>
-                                                {`Style: ${beer.style}`}
-                                            </Typography>
-                                        </CardContent>
-                                    </Box>
-                                </Card>
-                            </div>
-                        </ListItem>
-                    ))}
-                </List>
-            )}
-        </div>
+                {error && (
+                    <Typography variant="body1" color="error" margin="noraml">
+                        Error fetching data.
+                    </Typography>
+                )}
+                {allBeersData && (
+                    <List>
+                        {(searchKeywords ? filteredBeers : allBeersData.beers).map((beer, index) => (
+                            <ListItem key={index}>
+                                <div style={{marginBottom: '20px', width: '100%'}}>
+                                    <Card sx={{width: '260px', height:'170px'}}>
+                                        <Box sx={{height: "100%", display: 'flex', alignItems: 'center', backgroundColor: 'rgb(245, 222, 179)'}}>
+                                            <CardMedia
+                                                component="img"
+                                                sx={{width: 100, height: 100, objectFit: 'cover'}}
+                                                image={beerIcon}
+                                                alt={beer.name}
+                                            />
+                                            <CardContent>
+                                                <Typography variant="h6" component="div" sx={{textAlign: 'left', color: 'rgb(78, 42, 30)'}}>
+                                                    {`${beer.name}`}
+                                                </Typography>
+                                                <Typography variant="body2" color="textSecondary" sx={{textAlign: 'left', mt: 1, color: 'rgb(78, 42, 30)'}}>
+                                                    {`Style: ${beer.style}`}
+                                                </Typography>
+                                            </CardContent>
+                                        </Box>
+                                    </Card>
+                                </div>
+                            </ListItem>
+                        ))}
+                    </List>
+                )}
+            </Box>
+        </Box>
     );
 };
 
