5aa696d1d7d5560b0d3bd61e6a448ac5c68f8b99
Chiara Romanini <caromanini@miuandes.cl>
2024-09-10 15:29:49 -0300

    added attendance index

--- backend/app/models/attendance.rb
+++ backend/app/models/attendance.rb
@@ -1,6 +1,12 @@
 class Attendance < ApplicationRecord
+  respond_to :json
   belongs_to :user
   belongs_to :event
+
+  def index
+    @attendances = Attendance.all
+    render json: { attendances: @attendances }, status: :ok
+  end
 
   def check_in
     update(checked_in: true)
