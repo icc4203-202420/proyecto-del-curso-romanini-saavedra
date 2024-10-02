3fbf1e50d7a29573b31aca8db776df443c46358a
FabianSaavedraE <fasaavedra1@miuandes.cl>
2024-10-02 20:32:57 -0300

    Login and token functional

--- hybrid-frontend/App.js
+++ hybrid-frontend/App.js
@@ -1,33 +1,48 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import BeersScreen from './app/beers'
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from 'react-native';
+import BeersScreen from './app/beers';
 import BarsScreen from './app/bars';
 import ProfileScreen from './app/profile';
 import HomeScreen from './app/home';
+import LoginScreen from './app/auth/LoginScreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Bars" component={BarsScreen} />
+      <Tab.Screen name="Beers" component={BeersScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen}/>
-        <Tab.Screen name="Bars" component={BarsScreen} />
-        <Tab.Screen name="Beers" component={BeersScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainTabs"
+          component={MainTabs}
+          options={({ navigation }) => ({
+            title: 'My App',
+            headerRight: () => (
+              <Button
+                onPress={() => navigation.navigate('Login')}
+                title="Login"
+                color="#000"
+              />
+            ),
+          })}
+        />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
