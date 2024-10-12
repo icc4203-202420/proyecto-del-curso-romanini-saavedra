40d6698432f6e6d332275cca4859403fb548cce4
Chiara Romanini <caromanini@miuandes.cl>
2024-10-11 21:12:52 -0300

    added beer information

---
+++ hybrid-frontend/app/beers/BarsBeers.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    Button
+} from 'react-native';
+
+const BarsBeers = () => {
+    return (
+        <View>
+            <Text>Aca se muestran los bares que tienen esta cerveza</Text>
+        </View>
+    )
+}
+
+export default BarsBeers;
--- hybrid-frontend/app/beers/BeerDetails.js
+++ hybrid-frontend/app/beers/BeerDetails.js
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image,
+    Button
 } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Reviews from './Reviews';
+import BarsBeers from './BarsBeers';
+
+const beerBottleIcon = require('../../assets/images/beer_bottle_icon.png');
 
 const BeerDetails = ({route}) => {
-    const { beer } = route.params;
+    const { beer } = route.params;  
+    const [index, setIndex] = useState(0);
+    const [routes] = useState([
+        {key: 'information', title: 'Information'},
+        {key: 'reviews', title: 'Reviews'},
+        {key: 'availableAt', title: 'Available at'}
+    ]);
+
+    const renderScene = SceneMap({
+        information: () => (
+            <View style={{padding: 10}}>
+                <Text style={{fontweight: 'bold', color:'black'}}>Style: {beer.style}</Text>
+                <Text style={{color: 'black'}}>Alcohol: {beer.alcohol}</Text>
+                <Text style={{color: 'black'}}>Hops: {beer.hop}</Text>
+                <Text style={{color: 'black'}}>IBU: {beer.ibu}</Text>
+                <Text style={{color: 'black'}}>Yeast: {beer.yeast}</Text>
+                <Text style={{color: 'black'}}>Malts: {beer.malts}</Text>
+            </View>
+        ), 
+        reviews: () => (
+            <Reviews/>
+        ),
+        availableAt: () => (
+            <BarsBeers/>
+        )
+    });
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>{beer.name}</Text>
-            <Text style={styles.subtitle}>Style: {beer.style}</Text>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={beerBottleIcon}
+                    style={styles.beerImage}
+                    resizeMode="contain"
+                />
+                <View style={styles.overlayTextContainer}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Ionicons name="star" size={20} color="gold" />
+                        <Text style={{ marginLeft: 5 }}>
+                            {beer.avg_rating !== null ? beer.avg_rating : '0.0'}
+                        </Text>
+                    </View>
+                    <Text style={{fontSize: 36, fontWeight: 'bold'}}>{beer.name}</Text>
+                    <Text style={{fontSize: 18}}>Brewery: Lorem Ipsum Brewery</Text>
+                    <View style={styles.reviewButtonContainer}>
+                        <Button title="Review"/>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.tabViewContainer}>
+                <TabView
+                    navigationState={{ index, routes }}
+                    renderScene={renderScene}
+                    onIndexChange={setIndex}
+                    initialLayout={{ width: 300 }}
+                />
+            </View>
         </View>
     )
 }
@@ -19,8 +78,31 @@ const BeerDetails = ({route}) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
-        alignItems: 'center',
+      },
+      tabViewContainer: {
+        flex: 1
+      },
+      imageContainer: {
+        position: 'relative',
+        width: '100%',
+        height: 300,
+        flexDirection: 'column'
+      },   
+      reviewButtonContainer: {
+        top: 10,
+        width: 100,
+      },
+      beerImage: {
+        width: '100%',
+        height: 300,
+        opacity: 0.2,
+      },
+      overlayTextContainer: {
+        position: 'absolute',
+        bottom: 90,
+        left: 20,
+        right: 10,
+        flexDirection: 'column'
       },
       title: {
         fontSize: 24,
